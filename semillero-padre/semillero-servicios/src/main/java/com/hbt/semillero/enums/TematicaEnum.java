27a0307772e70f45d4babfa08889400165f9c488
Diego Ortiz <zitro13@gmail.com>
2022-08-25 19:25:24 -0500

    Se agrega clase comic y se mapea

---
+++ semillero-padre/semillero-servicios/src/main/java/com/hbt/semillero/enums/TematicaEnum.java
@@ -0,0 +1,38 @@
+/**
+ * TematicaEnum.java
+ */
+package com.hbt.semillero.enums;
+
+/**
+ * <b>Descripción:<b> Clase que determina
+ * <b>Caso de Uso:<b> Semillero2022
+ * @author Diego Armando Ortiz Bastidas
+ * @version 1.0
+ */
+public enum TematicaEnum {
+	
+	AVENTURAS("enum.tematica.aventuras"),
+	BELICO("enum.tematica.belico"), 
+	HUMORISTICO("enum.tematica.humoristico"), 
+	DEPORTIVO("enum.tematica.deportivo"), 
+	FANTASTICO("enum.tematica.fantastico"), 
+	CIENCIA_FICCION("enum.tematica.cianciaficcion"), 
+	HISTORICO("enum.tematica.historico"), 
+	HORROR("enum.tematica.horror"),
+	;
+	
+	private String etiqueta;
+	
+	TematicaEnum(String etiqueta){
+		this.etiqueta = etiqueta;
+	}
+
+	/**
+	 * Metodo encargado de retornar el valor del atributo etiqueta
+	 * @return El etiqueta asociado a la clase
+	 */
+	public String getEtiqueta() {
+		return etiqueta;
+	}
+	
+}
