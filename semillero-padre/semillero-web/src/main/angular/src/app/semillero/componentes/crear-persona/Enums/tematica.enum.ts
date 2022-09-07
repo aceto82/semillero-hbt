eecda1efa39267b27d4aee5ee84995e152374f58
Diego Ortiz <zitro13@gmail.com>
2022-09-06 19:16:20 -0500

    Ejemplos binding Angular

---
+++ semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/crear-persona/Enums/tematica.enum.ts
@@ -0,0 +1,7 @@
+export enum TematicaEnum{
+    HORROR="Horror",
+    AVENTURA="Aventura",
+    BELICO="Belico",
+    CIENCIA_FICCION="Ciencia_ficcion",
+    HUMORISTICO="Humoristico",
+}
--- semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/crear-persona/crear-persona.component.ts
+++ semillero-padre/semillero-web/src/main/angular/src/app/semillero/componentes/crear-persona/crear-persona.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EstadoEnum } from './Enums/estado.enum';
+import { TematicaEnum } from './Enums/tematica.enum';
+
 
 @Component({
   selector: 'app-crear-persona',
@@ -7,12 +10,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearPersonaComponent implements OnInit {
 
-  public saludo:string;
+  public saludo: string;
+
+  public comics: Array<any>;
+
+  public imagen: any;
 
   constructor() { }
 
   ngOnInit() {
     this.saludo = "Hola semillero 2022";
+    this.comics = this.crearComics();
+    let url = "https://apod.nasa.gov/apod/image/2202/AuroraPillars_Correia_960.jpg";
+    let height: number = 400;
+    let width: number = 300;
+    this.imagen = this.asignarImagen(url, height, width);
+  }
+
+  private asignarImagen(url: string, height: number, width: number): any {
+    return {
+      url: url,
+      height: height,
+      width: width,
+    };
+  }
+
+  private crearComics(): Array<any> {
+    let listaComics: Array<any>;
+    let comicSuperman: any = {
+      nombre: "Superman",
+      estado: EstadoEnum.ACTIVO,
+      precio: 120,
+      tematica: TematicaEnum.AVENTURA,
+      fechaVenta: null
+    }
+
+    let comicThor: any = {
+      nombre: "Thor",
+      estado: EstadoEnum.ACTIVO,
+      precio: 120,
+      tematica: TematicaEnum.AVENTURA,
+      fechaVenta: null
+    }
+
+    let comicSpiderman: any = {
+      nombre: "Spiderman",
+      estado: EstadoEnum.ACTIVO,
+      precio: 120,
+      tematica: TematicaEnum.AVENTURA,
+      fechaVenta: null
+    }
+
+    let comicHulk: any = {
+      nombre: "Hulk",
+      estado: EstadoEnum.ACTIVO,
+      precio: 120,
+      tematica: TematicaEnum.AVENTURA,
+      fechaVenta: null
+    }
+    listaComics = new Array<any>();
+    listaComics.push(comicSpiderman);
+    listaComics.push(comicThor);
+    listaComics.push(comicSpiderman);
+    listaComics.push(comicHulk);
+    return listaComics;
+  }
+
+  public mostrarDescripcionimagen(): void {
+    console.log(JSON.stringify(this.imagen));
+    alert("Info imagen " + JSON.stringify(this.imagen));
   }
 
 }
